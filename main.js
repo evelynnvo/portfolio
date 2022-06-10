c6e2c1c3704d01a0d7e33413edf3be7510ad3543
evelynnvo <evelynvo315@gmail.com>
2022-06-10 17:26:05 -0500

    aasdfasd

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const card = document.querySelector('.card-inner');
-card.addEventListener('hover', function () {
+card.addEventListener('click', function () {
     card.classList.toggle('is-flipped');
 });

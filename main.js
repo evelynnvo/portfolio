b811439694d1980bf193e3b2dbc83371615f9cb7
evelynnvo <evelynvo315@gmail.com>
2022-06-10 18:50:20 -0500

    adsasas

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const card = document.querySelector('.card-inner');
 card.addEventListener('click', function () {
     card.classList.toggle('is-flipped');
-    // const cardHeaderFront = document.querySelector('.card-header-front');
-    // cardHeaderFront.classList.toggle('is-flipped');
+    const cardHeaderFront = document.querySelector('.card-header-front');
+    cardHeaderFront.classList.toggle('is-flipped');
 });

c625e0ea650962652dc49d10679d9e45fa0632d4
evelynnvo <evelynvo315@gmail.com>
2022-06-10 18:01:01 -0500

    afadsa

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const card = document.querySelector('.card-inner');
 card.addEventListener('click', function () {
     card.classList.toggle('is-flipped');
+    card.getAttribute('.card-face-front').getAttribute('.card-content').getAttribute('.card-header-front').toggle('is-flipped');
 });

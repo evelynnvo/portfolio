8d6b52a8b1d7ef1aed464be6d89e060d10274a98
evelynnvo <evelynvo315@gmail.com>
2022-06-11 01:05:02 -0500

    sdfdfd

--- main.js
+++ main.js
@@ -3,52 +3,39 @@ var prev_card = 1; // number of next card to return
 var max_card = 5;
 
 const return_button = document.querySelector('.return-button');
-return_button.addEventListener('click', return_card());
-document.keyDown(function(e) {
-    if(e.keyCode === 37) {
-        return_card();
-    } else if(e.keyCode === 39) {
-        deal_card();
-    }
-});
-
-const deal_button = document.querySelector('.deal-button');
-deal_button.addEventListener('click', deal_card());
-
-function return_card() {
+return_button.addEventListener('click', function () {
     if (prev_card > 1) {
         $('.card-' + (prev_card - 1)).css({ 'visibility': 'visible' });
         $('.card-' + prev_card).animate({ top: '33em' }, "slow").promise().done(function () {
             $('.card-' + prev_card).css('visibility', 'hidden');
-                prev_card--;
-                next_card--;
-            if(next_card <= max_card) {
+            prev_card--;
+            next_card--;
+            if (next_card <= max_card) {
                 $('.deal-button').css('visibility', 'visible');
-            } 
+            }
 
-            if(prev_card === 1) {
+            if (prev_card === 1) {
                 $('.return-button').css('visibility', 'hidden');
             }
         });
     }
-    return;
-}
+});
 
-function deal_card() {
+const deal_button = document.querySelector('.deal-button');
+deal_button.addEventListener('click', function () {
     if (next_card < max_card + 1) {
         $('.card-' + next_card).css('visibility', 'visible');
         $('.card-' + next_card).animate({ top: '-33em' }, "slow").promise().done(function () {
             $('.card-' + prev_card).css({ 'visibility': 'hidden' });
             prev_card++;
             next_card++;
-            if(next_card > max_card) {
+            if (next_card > max_card) {
                 $('.deal-button').css('visibility', 'hidden');
             }
-            
-            if(prev_card > 1) {
+
+            if (prev_card > 1) {
                 $('.return-button').css('visibility', 'visible');
             }
         });
     }
-    return;
-}
+});

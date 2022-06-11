e1d54b4735162da05213fd61a91c421490a5b29a
evelynnvo <evelynvo315@gmail.com>
2022-06-10 23:46:20 -0500

    asdd

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 var next_card = 2; // number of next card to deal
 var prev_card = 1; // number of next card to return
+var max_card = 3;
 
 const return_button = document.querySelector('.return-button');
 return_button.addEventListener('click', function () {
-    $('.card-' + (prev_card - 1)).css({ 'visibility': 'visible' });
-    $('.card-' + prev_card).animate({top: '33em'}, "slow").promise().done(function() {
-        $('.card-' + prev_card).css('visibility','hidden');
-    });
-
-    prev_card--;
-    next_card--;
+    if (prev_card > 1) {
+        $('.card-' + (prev_card - 1)).css({ 'visibility': 'visible' });
+        $('.card-' + prev_card).animate({ top: '33em' }, "slow").promise().done(function () {
+            $('.card-' + prev_card).css('visibility', 'hidden');
+            prev_card--;
+            next_card--;
+        });
+    }
 });
 
 const deal_button = document.querySelector('.deal-button');
 deal_button.addEventListener('click', function () {
-    $('.card-' + next_card).css('visibility', 'visible');
-    $('.card-' + next_card).animate({top: '-33em'}, "slow").promise().done(function() {
+    if (next_card < 3) {
+        $('.card-' + next_card).css('visibility', 'visible');
+        $('.card-' + next_card).animate({ top: '-33em' }, "slow").promise().done(function () {
             $('.card-' + prev_card).css({ 'visibility': 'hidden' });
-    });
-
-    prev_card++;
-    next_card++;
+            prev_card++;
+            next_card++;
+        });
+    }
 });

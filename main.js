868b54ddc89ec3332e89386dcbcbf489644b12a3
evelynnvo <evelynvo315@gmail.com>
2022-06-10 21:02:53 -0500

    trying to hide bottom cards

--- main.js
+++ main.js
@@ -4,3 +4,11 @@
 //     const cardHeaderFront = document.querySelector('.card-header-front');
 //     cardHeaderFront.classList.toggle('is-flipped');
 // });
+
+var top_card = 2;
+var total_deck_cards = 2;
+for(let i = 1; i < total_deck_cards; i++) {
+    if (!document.getElementsByClassName(".card-" + top_card)) {
+        document.getElementsByClassName(".card-" + top_card).style.visibility = "hidden";
+    }
+}

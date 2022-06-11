e425e270205200d2bd22e259259219edac801381
evelynnvo <evelynvo315@gmail.com>
2022-06-10 23:00:40 -0500

    asdd

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const return_button = document.querySelector('.return-button');
 return_button.addEventListener('click', function () {
     // $('.card-2').css('visibility','hidden');
-    $('.card-2').css({ 'transform': 'translate(0,-4em)' });
-    $('.card-2').css({ 'transform': 'translate(0,-33.72em)' });
-    $('.card-2').css({ 'transition': 'transform 1s cubic-bezier(.25,0,.28,1.43)' });
-    $('.card-1').css({ 'visibility': 'hidden'});
-    $('.card-1').css({ 'transiition-delay': '.5s'});
+    // $('.card-2').css({ 'transform': 'translate(0,-4em)' });
+    // $('.card-2').css({ 'transform': 'translate(0,-33.72em)' });
+    // $('.card-2').css({ 'transition': 'transform 1s cubic-bezier(.25,0,.28,1.43)' });
+    
+    // $('.card-1').css({ 'visibility': 'hidden'});
+    $('.card-2').slideUp("slow", function() {
 
-
+    });
 });
 
 const deal_button = document.querySelector('.deal-button');

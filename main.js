cacc484d385a611b176415d37b601292ade646d1
evelynnvo <evelynvo315@gmail.com>
2022-06-10 23:35:26 -0500

    aaasdf

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
+var next_card = 2; // number of next card to deal
+var prev_card = 1; // number of next card to return
+
 const return_button = document.querySelector('.return-button');
 return_button.addEventListener('click', function () {
-    $('.card-1').css({ 'visibility': 'visible' });
-    $('.card-2').animate({top: '33em'}).promise().done(function() {
-        $('.card-2').css('visibility','hidden');
+    $('.card-' + (prev_card - 1)).css({ 'visibility': 'visible' });
+    $('.card-' + prev_card).animate({top: '33em'}, "slow").promise().done(function() {
+        $('.card-' + prev_card).css('visibility','hidden');
     });
+
+    prev_card--;
+    next_card--;
 });
 
 const deal_button = document.querySelector('.deal-button');
 deal_button.addEventListener('click', function () {
-    $('.card-2').css('visibility', 'visible');
-    $('.card-2').animate({top: '-33em'}).promise().done(function() {
-            $('.card-1').css({ 'visibility': 'hidden' });
+    $('.card-' + next_card).css('visibility', 'visible');
+    $('.card-' + prev_card).animate({top: '-33em'}, "slow").promise().done(function() {
+            $('.card-' + prev_card).css({ 'visibility': 'hidden' });
     });
+
+    prev_card++;
+    next_card++;
 });

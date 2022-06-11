57092f6ae26ee2652dc225687b9b0c9911f0d960
evelynnvo <evelynvo315@gmail.com>
2022-06-10 23:22:07 -0500

    asdfs

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const return_button = document.querySelector('.return-button');
 return_button.addEventListener('click', function () {
     $('.card-2').css('visibility','hidden');
+    $('.card-1').css({ 'visibility': 'visible' });
+    $('.card-2').animate({top: '33em'}).promise().done(function() {
+    });
 });
 
 const deal_button = document.querySelector('.deal-button');
@@ -8,6 +11,5 @@ deal_button.addEventListener('click', function () {
     $('.card-2').css('visibility', 'visible');
     $('.card-2').animate({top: '-33em'}).promise().done(function() {
             $('.card-1').css({ 'visibility': 'hidden' });
-
     });
 });

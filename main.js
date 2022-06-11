e36b13e43a96f005462ff72dd87cfa26d64b563b
evelynnvo <evelynvo315@gmail.com>
2022-06-11 00:57:42 -0500

    asdf

--- main.js
+++ main.js
@@ -3,7 +3,19 @@ var prev_card = 1; // number of next card to return
 var max_card = 5;
 
 const return_button = document.querySelector('.return-button');
-return_button.addEventListener('click', function () {
+return_button.addEventListener('click', return_card());
+$(document).keyDown(function(e) {
+    if(e.keyCode === 37) {
+        return_card();
+    } else if(e.keyCode === 39) {
+        deal_card();
+    }
+});
+
+const deal_button = document.querySelector('.deal-button');
+deal_button.addEventListener('click', deal_card());
+
+function return_card() {
     if (prev_card > 1) {
         $('.card-' + (prev_card - 1)).css({ 'visibility': 'visible' });
         $('.card-' + prev_card).animate({ top: '33em' }, "slow").promise().done(function () {
@@ -19,10 +31,9 @@ return_button.addEventListener('click', function () {
             }
         });
     }
-});
+}
 
-const deal_button = document.querySelector('.deal-button');
-deal_button.addEventListener('click', function () {
+function deal_card() {
     if (next_card < max_card + 1) {
         $('.card-' + next_card).css('visibility', 'visible');
         $('.card-' + next_card).animate({ top: '-33em' }, "slow").promise().done(function () {
@@ -38,4 +49,4 @@ deal_button.addEventListener('click', function () {
             }
         });
     }
-});
+}

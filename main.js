12e41fb2d07a4218d05b8660fb85643189f15c65
evelynnvo <evelynvo315@gmail.com>
2022-06-10 22:39:17 -0500

    animation

--- main.js
+++ main.js
@@ -1,26 +1,10 @@
-// const card = document.querySelector('.card-inner');
-// card.addEventListener('click', function () {
-//     card.classList.toggle('is-flipped');
-//     const cardHeaderFront = document.querySelector('.card-header-front');
-//     cardHeaderFront.classList.toggle('is-flipped');
-// });
-
 const return_button = document.querySelector('.return-button');
 return_button.addEventListener('click', function() {
     $('.card-2').css('visibility','hidden');
+    $('.card-2').css({'transform': 'translate(0,4em)'});
 });
 
 const deal_button = document.querySelector('.deal-button');
 deal_button.addEventListener('click', function() {
     $('.card-2').css('visibility','visible');
 });
-
-// deal_button.addEventListener('click', function() {
-//     document.getElementsByClassName('.card-2')[0].style.visibility = "visible";
-//     deal_button.style.background = "blue";
-
-// });
-
-// $('.return-button').click(function() {
-//     $('.card-2').css('visibility', 'visible');
-// });

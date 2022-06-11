98beca23495c400682d262dd110cdd7887cf508c
evelynnvo <evelynvo315@gmail.com>
2022-06-10 22:42:30 -0500

    asdf

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const return_button = document.querySelector('.return-button');
 return_button.addEventListener('click', function() {
-    $('.card-2').css('visibility','hidden');
-    $('.card-2').css({'transform': 'translate(0,4em)'});
+    // $('.card-2').css('visibility','hidden');
+    $('.card-2').css({'transform': 'translate(0,-4em)'});
 });
 
 const deal_button = document.querySelector('.deal-button');

2a08f8133ceecaf458c1530c16fa3abd9bb1539c
evelynnvo <evelynvo315@gmail.com>
2022-06-10 23:08:07 -0500

    asdf

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 const return_button = document.querySelector('.return-button');
 return_button.addEventListener('click', function () {
     // $('.card-2').css('visibility','hidden');
-    // $('.card-2').css({ 'transform': 'translate(0,-4em)' });
-    // $('.card-2').css({ 'transform': 'translate(0,-33.72em)' });
-    // $('.card-2').css({ 'transition': 'transform 1s cubic-bezier(.25,0,.28,1.43)' });
-    
-    // $('.card-1').css({ 'visibility': 'hidden'});
-    $('.card-2').slideUp("slow", function() {
-
+    $('.card-2').css({ 'transform': 'translate(0,-4em)' });
+    $('.card-2').css({ 'transform': 'translate(0,-33.72em)' });
+    $('.card-2').css({ 'transition': 'transform 1s cubic-bezier(.25,0,.28,1.43)' }).promise().done(function () {
+        $('.card-1').css({ 'visibility': 'hidden' });
     });
+
 });
 
 const deal_button = document.querySelector('.deal-button');

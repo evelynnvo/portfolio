2cd8966c6138bf8773c5a54d3ebd6a7881aeafea
evelynnvo <evelynvo315@gmail.com>
2022-06-10 22:17:53 -0500

    afaa

--- main.js
+++ main.js
@@ -8,16 +8,16 @@
 const return_button = document.querySelector('.return-button');
 
 return_button.addEventListener('click', function() {
-    ​$('.card-2').css('visibility'​​​​​​​​​​​​​​​​​​​​​​​​​​​,'visible');​​​​​​
+    $('.card-2').css('visibility','visible');
 });
 
-const deal_button = document.querySelector('.deal-button');
-deal_button.addEventListener('click', function() {
-    document.getElementsByClassName('.card-2')[0].style.visibility = "visible";
-    deal_button.style.background = "blue";
+// const deal_button = document.querySelector('.deal-button');
+// deal_button.addEventListener('click', function() {
+//     document.getElementsByClassName('.card-2')[0].style.visibility = "visible";
+//     deal_button.style.background = "blue";
 
-});
+// });
 
-$('.return-button').click(function() {
-    $('.card-2').css('visibility', 'visible');
-});
+// $('.return-button').click(function() {
+//     $('.card-2').css('visibility', 'visible');
+// });
